a5685ae1ca90cefaea131ab1b71de5849c774be3
Austyn Deseo <Austyn@admiralfeb.dev>
2024-12-03 18:24:30 -0600

    add selector for 2024

--- 2024/src/days/mod.rs
+++ 2024/src/days/mod.rs
@@ -1,3 +1,54 @@
+use std::{
+    error::Error,
+    fmt::{self, Display, Formatter},
+    io::stdin,
+};
+
+use common::day::AdventDay;
+
 pub mod day1;
 pub mod day2;
 pub mod day3;
+
+fn run_day(input: i32) -> Result<(), Box<dyn Error>> {
+    match input {
+        1 => Ok(day1::Day1.run()),
+        2 => Ok(day2::Day2.run()),
+        3 => Ok(day3::Day3.run()),
+        _ => Err(Box::new(DayNotFoundError)),
+    }
+}
+
+pub fn select_day(input: Option<i32>) -> Result<(), Box<dyn Error>> {
+    if let Some(input_day) = input {
+        return run_day(input_day);
+    }
+
+    let days_to_select = vec![1, 2, 3];
+    println!(
+        "Please select a day to run (1-{})",
+        days_to_select.last().unwrap()
+    );
+    let mut selected_day = String::new();
+    match stdin().read_line(&mut selected_day) {
+        Ok(_) => {
+            let day = selected_day.trim().parse::<i32>()?;
+            run_day(day)
+        }
+        Err(_) => Err(Box::new(std::io::Error::new(
+            std::io::ErrorKind::InvalidInput,
+            "Error reading input",
+        ))),
+    }
+}
+
+#[derive(Debug)]
+struct DayNotFoundError;
+
+impl Error for DayNotFoundError {}
+
+impl Display for DayNotFoundError {
+    fn fmt(&self, f: &mut Formatter<'_>) -> fmt::Result {
+        write!(f, "Day not found. Select another day.")
+    }
+}
--- 2024/src/main.rs
+++ 2024/src/main.rs
@@ -2,11 +2,23 @@
 
 mod days;
 
-use common::day::AdventDay;
-use days::*;
+use std::env;
 
 fn main() {
-    let day = day3::Day3;
+    let args: Vec<String> = env::args().collect();
+    // dbg!(args);
 
-    day.run();
+    let day_to_run_option = args.get(1);
+    if let Some(day_to_run) = day_to_run_option {
+        let running_day: i32 = day_to_run.parse().expect("arg must be an i32");
+        let result = days::select_day(Some(running_day));
+        if let Err(error_result) = result {
+            println!("Error: {}", error_result);
+        }
+    } else {
+        let result = days::select_day(None);
+        if let Err(error_result) = result {
+            println!("Error: {}", error_result);
+        }
+    }
 }
