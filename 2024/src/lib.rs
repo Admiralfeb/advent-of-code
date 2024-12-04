3a3764db1edb18cec7d3978211bb688090e3666c
Austyn Deseo <Austyn@admiralfeb.dev>
2024-12-03 22:50:52 -0600

    generalize day selection

--- 2024/src/lib.rs
+++ 2024/src/lib.rs
@@ -1,19 +1,34 @@
 #![allow(dead_code)]
 
-use common::year::AdventYear;
+use std::error;
+
+use common::{
+    day::AdventDay,
+    year::{AdventYear, DayNotFoundError},
+};
 
 mod common_values;
 pub mod days;
+use days::*;
 
 pub struct Year2024;
 
 impl AdventYear for Year2024 {
-    fn run(&self, input_day: Option<i32>) -> Result<(), Box<dyn std::error::Error>> {
-        let result = days::select_day(input_day)?;
+    fn run(&self, input_day: Option<i32>) -> Result<(), Box<dyn error::Error>> {
+        let result = self.select_day(input_day)?;
 
         println!("year 2024 {}", result);
         Ok(())
     }
+    fn get_days(&self) -> Vec<i32> {
+        vec![1, 2, 3]
+    }
+    fn run_day(&self, input: i32) -> Result<String, Box<dyn error::Error>> {
+        match input {
+            1 => Ok(day1::Day1.run()),
+            2 => Ok(day2::Day2.run()),
+            3 => Ok(day3::Day3.run()),
+            _ => Err(Box::new(DayNotFoundError)),
+        }
+    }
 }
-
-impl Year2024 {}
--- common/src/year.rs
+++ common/src/year.rs
@@ -1,7 +1,44 @@
+use std::{error, fmt, io};
+
 pub fn get_years() -> Vec<i32> {
     vec![2023, 2024]
 }
 
 pub trait AdventYear {
-    fn run(&self, input: Option<i32>) -> Result<(), Box<dyn std::error::Error>>;
+    fn run(&self, input: Option<i32>) -> Result<(), Box<dyn error::Error>>;
+    fn select_day(&self, input: Option<i32>) -> Result<String, Box<dyn error::Error>> {
+        if let Some(input_day) = input {
+            return self.run_day(input_day);
+        }
+
+        let days_to_select = self.get_days();
+        println!(
+            "Please select a day to run (1-{})",
+            days_to_select.last().unwrap()
+        );
+        let mut selected_day = String::new();
+        match io::stdin().read_line(&mut selected_day) {
+            Ok(_) => {
+                let day = selected_day.trim().parse::<i32>()?;
+                self.run_day(day)
+            }
+            Err(_) => Err(Box::new(std::io::Error::new(
+                std::io::ErrorKind::InvalidInput,
+                "Error reading input",
+            ))),
+        }
+    }
+    fn get_days(&self) -> Vec<i32>;
+    fn run_day(&self, input: i32) -> Result<String, Box<dyn error::Error>>;
+}
+
+#[derive(Debug)]
+pub struct DayNotFoundError;
+
+impl error::Error for DayNotFoundError {}
+
+impl fmt::Display for DayNotFoundError {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        write!(f, "Day not found. Select another day.")
+    }
 }
