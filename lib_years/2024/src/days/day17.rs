ebd6f3e4f4fcdd82c2d3f94a88b00c6578a1e1c8
Austyn Deseo <Austyn@admiralfeb.dev>
2024-12-16 23:00:37 -0600

    init all days

---
+++ lib_years/2024/src/days/day17.rs
@@ -0,0 +1,45 @@
+use common::{day::AdventDay, file::read_file};
+use std::{error::Error, fmt};
+
+use crate::common_values::get_data_path;
+
+pub struct Day17;
+impl AdventDay for Day17 {
+    fn puzzle1(&self, path: &str) -> Result<impl fmt::Debug, Box<dyn Error>> {
+        let binding = read_file(path)?;
+        Ok("")
+    }
+
+    fn puzzle2(&self, path: &str) -> Result<impl fmt::Debug, Box<dyn Error>> {
+        Ok("")
+    }
+    fn run(&self) -> String {
+        let data_path = get_data_path().to_string();
+        let day1_path = data_path + "day17.txt";
+        self.print_puzzles(6, day1_path.as_str(), day1_path.as_str())
+    }
+}
+
+#[cfg(test)]
+mod test {
+
+    use super::*;
+
+    #[test]
+    fn test_puzzle1() {
+        let path = "data/day17-test.txt";
+        let day = Day17;
+        let result = day.puzzle1(path).unwrap();
+        let expected: i64 = 11;
+        assert_eq!(expected.to_string(), format!("{:?}", result))
+    }
+
+    #[test]
+    fn test_puzzle2() {
+        let path = "data/day17-test.txt";
+        let day = Day17;
+        let result = day.puzzle2(path).unwrap();
+        let expected: i64 = 31;
+        assert_eq!(expected.to_string(), format!("{:?}", result))
+    }
+}
--- lib_years/2024/src/lib.rs
+++ lib_years/2024/src/lib.rs
@@ -21,7 +21,10 @@ impl AdventYear for Year2024 {
         Ok(())
     }
     fn get_days(&self) -> Vec<i32> {
-        vec![1, 2, 3, 4, 5, 6]
+        vec![
+            1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24,
+            25,
+        ]
     }
     fn run_day(&self, input: i32) -> Result<String, Box<dyn error::Error>> {
         match input {
@@ -30,7 +33,26 @@ impl AdventYear for Year2024 {
             3 => Ok(day3::Day3.run()),
             4 => Ok(day4::Day4.run()),
             5 => Ok(day5::Day5.run()),
-            5 => Ok(day6::Day6.run()),
+            6 => Ok(day6::Day6.run()),
+            7 => Ok(day7::Day7.run()),
+            8 => Ok(day8::Day8.run()),
+            9 => Ok(day9::Day9.run()),
+            10 => Ok(day10::Day10.run()),
+            11 => Ok(day11::Day11.run()),
+            12 => Ok(day12::Day12.run()),
+            13 => Ok(day13::Day13.run()),
+            14 => Ok(day14::Day14.run()),
+            15 => Ok(day15::Day15.run()),
+            16 => Ok(day16::Day16.run()),
+            17 => Ok(day17::Day17.run()),
+            18 => Ok(day18::Day18.run()),
+            19 => Ok(day19::Day19.run()),
+            20 => Ok(day20::Day20.run()),
+            21 => Ok(day21::Day21.run()),
+            22 => Ok(day22::Day22.run()),
+            23 => Ok(day23::Day23.run()),
+            24 => Ok(day24::Day24.run()),
+            25 => Ok(day25::Day25.run()),
             _ => Err(Box::new(DayNotFoundError)),
         }
     }
